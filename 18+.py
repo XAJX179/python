8bb404aa02abe6fd858aba3b0c725009d987e5e8
XAJX <xajx179@gmail.com>
2024-03-13 23:55:47 +0530

    new file:   18+.py 	new file:   ifelse.py

---
+++ 18+.py
@@ -0,0 +1,6 @@
+Age = int(input("Enter your age ,dont lie: "))
+
+if Age<69:
+    print('U r still a small pp')
+else:
+    print("U r amazing the god himself")
---
+++ ifelse.py
@@ -0,0 +1,9 @@
+a=1
+while a != 0:
+    a = int(input('enter a number:'))
+    if a > 0:
+        print("a is positive")
+    if a < 0:
+        print("a is negative")
+if a == 0:
+        print("a is zero",'the end')
