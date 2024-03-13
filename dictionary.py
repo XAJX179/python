b964f096d2f42ebd579550feaeccffe1e8ffe38d
XAJX <xajx179@gmail.com>
2024-03-13 19:51:26 +0530

    Changes to be committed: 	new file:   dictionary.py
    Changes not staged for commit:
    	modified:   dictionary.py
    	modified:   tempCodeRunnerFile.py

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+MyDictionary = {
+    'Name': 'Person Identity',
+    'Age': 'How old a person is',
+    'Clothes': 'What we Wear To cover body',
+    'Marks': [1, 2, 5, 5]
+
+}
+print(MyDictionary['Name'])
+print(MyDictionary['Age'])
+print(MyDictionary['Clothes'])
+print(MyDictionary['Marks'])
