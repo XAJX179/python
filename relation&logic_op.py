ea014c08bdbf4414501b8ca6431039ba97c7e272
XAJX <xajx179@gmail.com>
2024-03-14 12:26:30 +0530

    new file:   relation&logic_op.py

---
+++ relation&logic_op.py
@@ -0,0 +1,10 @@
+num = int(input("Enter a number: "))
+if num>10 and num>20:
+    print("The Value is greater than 10 & 20")
+if num>10 or num<30:
+    print("The Value is Greater than 10 or smaller than 30")
+if num!=0:
+    print("Not Zero")
+if not(num!=0):
+    print("Zero")
+#try entering 0
