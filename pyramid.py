6f7e5b0fe01e42a1ef697dfc2d8009f52d22a233
XAJX <xajx179@gmail.com>
2024-03-14 20:58:38 +0530

    new file:   pyramid.py

---
+++ pyramid.py
@@ -0,0 +1,10 @@
+for firstr in range(4):
+    print('*'*(firstr+1))
+
+print('')
+l = int(input("Enter Bitch 	୧༼ಠ益ಠ༽︻╦╤─  :"))
+
+for i in range(l):
+    print(" "*(l-i-1),end="")
+    print("*"*(i*2+1),end="")
+    print(" "*(l-i-1),)
