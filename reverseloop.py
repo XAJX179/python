6ddad05c246a647a14bb91fa99d77b631c6f1e3c
XAJX <xajx179@gmail.com>
2024-03-14 23:29:11 +0530

    new file:   reverseloop.py

---
+++ reverseloop.py
@@ -0,0 +1,21 @@
+num = int(input("Enter no. :"))
+
+for table in range(10,0,-1):
+    pass
+    print(f"{num} X {table} =  {num*table}")
+
+a1= num * 1
+a2= num * 2
+a3= num * 3
+a4= num * 4
+a5= num * 5
+a6= num * 6
+a7= num * 7
+a8= num * 8
+a9= num * 9
+a10= num * 10
+
+listy = [a1,a2,a3,a4,a5,a6,a7,a8,a9,a10]
+
+listy.reverse()
+print(listy)
