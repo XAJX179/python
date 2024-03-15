cef133563bd09ecdfe99a57aa7a9625322d1be2f
XAJX <xajx179@gmail.com>
2024-03-15 17:37:52 +0530

    modified:   greatestNoFunc.py
    Untracked files:
    	.gitignore

--- greatestNoFunc.py
+++ greatestNoFunc.py
@@ -1,12 +1,16 @@
-a1 = int(input("Enter Number 1: "))
-a2 = int(input("Enter Number 2: "))
-a3 = int(input("Enter Number 3: "))
+n1 = int(input("Enter Number 1: "))
+n2 = int(input("Enter Number 2: "))
+n3 = int(input("Enter Number 3: "))
 
-if a1 >= a2 and a1>=a3:
-    greatest= a1
-if  a2 >= a1 and a2>=a3:
-    greatest=a2
-if a3 >= a1 and a3>=a2:
-    greatest=a3
+def great(a1,a2,a3):
+    greatest=0
+    if a1 >= a2 and a1>=a3:
+        return a1
+    if  a2 >= a1 and a2>=a3:
+        return a2
+    if a3 >= a1 and a3>=a2:
+        return a3
+
+greatest= great(n1,n2,n3)
 
 print(f"{greatest} is the greatest!  ʕノ•ᴥ•ʔノ ︵ ┻━┻  ")
