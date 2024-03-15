a2d802c9778d71c4b965c6ff8907f6e04f5708de
XAJX <xajx179@gmail.com>
2024-03-15 16:49:12 +0530

    new file:   factoFunc.py 	new file:   greatestNoFunc.py

---
+++ factoFunc.py
@@ -0,0 +1,28 @@
+#for recursive
+#n! = 1*2*3*4*...*n
+#n! = 1*2*3*4*... *n-1 *n
+#n! = (n-1)!*n
+#4! = 1*2*3*4
+#3! = 1*2*3
+#4! = 3!*4
+
+def factfunc(n):
+    sum = 1
+    for i in range(1,n+1):
+        sum = sum * i
+    return print(sum)
+
+# factfunc(int(input("Enter No.  ⤜(ⱺ ʖ̯ⱺ)⤏  : ")))
+
+def factorecursive(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorecursive(n-1)
+
+print(input("Enter a no.  ⤜(ⱺ ʖ̯ⱺ)⤏ :"))
+
+# def hell():
+#     print("hello world!")
+#     return hell()
+# hell() 
+#bad recursion example
---
+++ greatestNoFunc.py
@@ -0,0 +1,12 @@
+a1 = int(input("Enter Number 1: "))
+a2 = int(input("Enter Number 2: "))
+a3 = int(input("Enter Number 3: "))
+
+if a1 >= a2 and a1>=a3:
+    greatest= a1
+if  a2 >= a1 and a2>=a3:
+    greatest=a2
+if a3 >= a1 and a3>=a2:
+    greatest=a3
+
+print(f"{greatest} is the greatest!  ʕノ•ᴥ•ʔノ ︵ ┻━┻  ")
