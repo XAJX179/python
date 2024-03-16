753cb73b92ed05eb91cb49646968c9b3a643ecaf
XAJX <xajx179@gmail.com>
2024-03-16 13:02:44 +0530

    new file:   MultiTbFunc.py

---
+++ MultiTbFunc.py
@@ -0,0 +1,10 @@
+def _Multi_Table(number):
+    n = 1
+    while True:
+        print(f"{number} X {n} = {number*n} ")
+        n = n+1
+        if n == 11:
+            break
+
+_Multi_Table(10)
+    
