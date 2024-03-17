0626a13962be92b69a91aee18104d8740e01f1f2
XAJX <xajx179@gmail.com>
2024-03-17 21:39:30 +0530

    new file:   oops/03_game_oops.py

---
+++ oops/03_game_oops.py
@@ -0,0 +1,25 @@
+class Remote:
+    def isLeftPressed(self):
+        pass
+    def isRightPressed(self):
+        pass
+    def isUpPressed(self):
+        pass
+    def isDownPressed(self):
+        pass
+
+class Player:
+    def moveUp(self):
+        pass
+    def moveDown(self):
+        pass
+    def moveLeft(self):
+        pass
+    def moveRight(self):
+        pass
+
+remote1 = Remote()
+player1 = Player()
+
+if (remote1.isLeftPressed()):
+    player1.moveLeft
