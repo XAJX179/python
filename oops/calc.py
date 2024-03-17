820fc7d724fd769db80b03b56eba50a9a825cc50
XAJX <xajx179@gmail.com>
2024-03-18 00:51:09 +0530

    new file:   oops/calc.py

---
+++ oops/calc.py
@@ -0,0 +1,8 @@
+class Number:
+    def __init__(self,num):
+        print(f"Square : {num ** 2}")
+        print(f"Cube : {num ** 3}")
+        print(f"Sqroot : {num ** 0.5}")
+
+
+num1 = Number(16)
