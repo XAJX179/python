b4a410ae799e6a8cc1ea494553a5a602c4380ac7
XAJX <xajx179@gmail.com>
2024-03-18 00:59:27 +0530

    modified:   oops/calc.py

--- oops/calc.py
+++ oops/calc.py
@@ -1,8 +1,17 @@
 class Number:
     def __init__(self,num):
-        print(f"Square : {num ** 2}")
-        print(f"Cube : {num ** 3}")
-        print(f"Sqroot : {num ** 0.5}")
+        self.number=num
+        self.cube()
+        self.square()
+     
+
+    def square(self):
+        print(f"Square : {self.number ** 2}")
+    def cube(self):    
+        print(f"Cube : {self.number ** 3}")
+    def sqroot(self): 
+        print(f"Sqroot : {self.number ** 0.5}")
 
 
 num1 = Number(16)
+num1.sqroot()
