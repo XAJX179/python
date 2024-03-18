2bdeb209693f86f31cfec8032b7e611a5efc6b7e
XAJX <xajx179@gmail.com>
2024-03-18 23:09:56 +0530

    new file:   oops/messing_self.py

--- oops/inheritance/01_inheritance.py
+++ oops/inheritance/01_inheritance.py
@@ -4,7 +4,13 @@ class Employee:
     def showDetails(self):
         print("This Is An Employee")
 
-class Programmer:
+class Programmer(Employee):
     lang= "Python"
     def getLang(self):
         print(f"Lang : {self.lang}")
+
+xajx = Employee()
+print(xajx.company)
+xajx2 = Programmer()
+print(xajx2.company)
+xajx2.getLang()
---
+++ oops/messing_self.py
@@ -0,0 +1,8 @@
+class sample:
+    a = "Honey"
+    def __init__(slf,name) :
+        slf.name = name
+
+
+obj = sample("XAJX")
+print(obj.name)
