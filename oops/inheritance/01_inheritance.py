b2f52a70d828eb39d88106699c172d033f8494b8
XAJX <xajx179@gmail.com>
2024-03-18 23:09:39 +0530

    new file:   oops/inheritance/01_inheritance.py

---
+++ oops/inheritance/01_inheritance.py
@@ -0,0 +1,10 @@
+class Employee:
+    company= "Google"
+
+    def showDetails(self):
+        print("This Is An Employee")
+
+class Programmer:
+    lang= "Python"
+    def getLang(self):
+        print(f"Lang : {self.lang}")
